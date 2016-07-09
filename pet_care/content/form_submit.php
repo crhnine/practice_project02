6206072845c96362744d1d7ce7640a318582b58e
crhnine <hillviewpcrepair@gmail.com>
2016-07-09 01:54:03 -0400

    Initial push of content

---
+++ pet_care/content/form_submit.php
@@ -0,0 +1,23 @@
+<?php
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$pets_name = $_POST['pets_name'];
+$pet_traits = $_POST['pet_traits'];
+$pet_type = $_POST['pet_type'];
+$pet_gender = $_POST['pet_gender'];
+$plan_level = $_POST['plan_levle'];
+$email_list = $_POST['email_list'];
+?>
+
+
+<?php  echo $first_name; echo '<br />' ?>
+<?php  echo $last_name; echo '<br />' ?>
+<?php  echo $pets_name; echo '<br />' ?>
+<?php  echo $pet_traits; echo '<br />' ?>
+<?php  echo $pet_type; echo '<br />' ?>
+<?php  echo $pet_gender; echo '<br />' ?>
+<?php  echo $plan_level; echo '<br />' ?>
+<?php  echo $email_list; echo '<br />' ?>
+
+<?php echo '<a href="../index.html">Home</a><br />' ?>
+<?php echo '<a href="contact.html">Contact</a><br />' ?>
